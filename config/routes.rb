8548220e9653153a4c3eba2274d120aa94fcf3c9
Sota3110 <144866058+Soa1208@users.noreply.github.com>
2024-01-12 11:54:45 +0900

    トップページをルートパスに設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  # root to: 'homes#top'
   get 'books/index'
   get 'books/show'
   get 'books/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books
 end
+
+# コメントアウトは確定
