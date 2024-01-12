a7371082bf5ab126401d1f286da6a0d49ef978a7
Sota3110 <144866058+Soa1208@users.noreply.github.com>
2024-01-12 14:42:42 +0900

    ルート設定 トップページから一覧への移動設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # root to: 'homes#top'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  
+  # get 'books' => 'books#index'
+  # get 'books' => 'books#new', as 'new_book'
+  # post 'books' => 'books#create'
+  
+  # get 'books/:id' => 'books#show', as 'book'
+  # patch 'books/:id' => 'books#update', as 'update_book'
+  # delete 'books/:id' => 'books#destroy', as 'destroy_book'
+  
+  # get 'books/:id/edit' => 'books#edit', as 'edit_book'
   resources :books
 end
-
-# コメントアウトは確定
