8acee861a8135ca29470fab21c009f2ffc0eca91
Sota3110 <144866058+Soa1208@users.noreply.github.com>
2024-01-12 15:48:44 +0900

    保存機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'books' => 'books#index'
-  
+  get 'books' => 'books#new'
   post 'books' => 'books#create'
+  
+  get 'books/:id' => 'book#show'
+  
   resources :books
 end
