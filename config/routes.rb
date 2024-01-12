77a9b5d306d3cd9580edf7a0f146666d2895c5df
Sota3110 <144866058+Soa1208@users.noreply.github.com>
2024-01-12 15:05:49 +0900

    新規投稿機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  # root to: 'homes#top'
+  root to: 'homes#top'
+  get 'books' => 'books#index'
   
-  # get 'books' => 'books#index'
-  # get 'books' => 'books#new', as 'new_book'
-  # post 'books' => 'books#create'
-  
-  # get 'books/:id' => 'books#show', as 'book'
-  # patch 'books/:id' => 'books#update', as 'update_book'
-  # delete 'books/:id' => 'books#destroy', as 'destroy_book'
-  
-  # get 'books/:id/edit' => 'books#edit', as 'edit_book'
+  post 'books' => 'books#create'
   resources :books
 end
