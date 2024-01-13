55c2eb8529bc7bc57e27da1fa8892d11c309cd20
Sota3110 <144866058+Soa1208@users.noreply.github.com>
2024-01-13 14:01:03 +0900

    削除機能の実装・一覧から詳細、編集画面への移動設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
   
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -27,6 +28,11 @@ class BooksController < ApplicationController
     redirect_to book_path(book.id)
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
   
   private
   def book_params
