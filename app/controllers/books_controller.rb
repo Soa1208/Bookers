31e942fce1869ac4699d6910f187dbe045db0ec3
Sota3110 <144866058+Soa1208@users.noreply.github.com>
2024-01-13 23:00:50 +0900

    テストエラー未改善

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,7 +20,7 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
+     @book = Book.find(params[:id])
   end
 
   def edit
@@ -38,8 +38,8 @@ class BooksController < ApplicationController
   end
   
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to books_path
   end
   
