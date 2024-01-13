c072db3c041ba06bb5bcfa8456a1fe9bf7c2e9ef
Sota3110 <144866058+Soa1208@users.noreply.github.com>
2024-01-13 10:48:50 +0900

    編集フォームと更新機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get 'books' => 'books#index'
-  get 'books' => 'books#new'
-  post 'books' => 'books#create'
-  get 'books/:id' => 'books#show', as: 'book'
   resources :books
 end
